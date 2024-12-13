69b7871b84fab97bfb1354e24eca422f58eabb7d
5er3jaM0l0d3c <telephonecizerwed@gmail.com>
2024-12-14 00:43:23 +0300

    Написал просмотр информации о точке локации животных

---
+++ DripChip/Controllers/LocationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interface;
+
+namespace DripChipAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private ILocation Location { get; set; }
+        public LocationController(ILocation location)
+        {
+            Location = location;
+        }
+
+        [Authorize]
+        [HttpGet("/locatins/{pointId}")]
+        public IActionResult GetLocationInfo(long? pointId)
+        {
+            if (pointId == null || pointId <= 0)
+            {
+                return StatusCode(400, "pointId = null,\npointId <= 0");
+            }
+
+            var result = Location.GetLocationInfo(pointId);
+
+            if (result == null)
+                return StatusCode(404, "Точка локации с таким pointId = " + pointId + " не найдена");
+            return new JsonResult(result);
+        }
+    }
+}
---
+++ Services/Service/LocationServices.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Service
+{
+    public class LocationServices : ILocation
+    {
+        private DripChipContext context {  get; set; }
+        public LocationServices(DripChipContext context)
+        {
+            this.context = context;
+        }
+
+        public Location? GetLocationInfo(long? id)
+        {
+            return context.Location.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
